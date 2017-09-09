25af5ee1982f3e654d886c0de4d73f2785d765bf
tns250cpp <31677719+tns250cpp@users.noreply.github.com>
2017-09-09 23:35:54 +0900

    1st assignment

---
+++ C_1st_Assignment.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	printf("1. 2017301071\n");
+	printf("2. ¡∂ªÛ¡ÿ\n");
+	printf("3. 010-4364-0488\n");
+	printf("4. https://github.com/tns250cpp \n");
+
+	return 0;
+}
